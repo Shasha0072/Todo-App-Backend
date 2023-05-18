7cb76e9002557950f3c1e5b20e1fa9dd6017a4f1
Shashwat Adhau <shashwatadhau3@gmail.com>
2023-05-18 18:11:21 +0530

    Code refactoring

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const morgan = require('morgan');
-const globalErrorHandler = require('./controllers/errorController');
+const globalErrorHandler = require('./controller/errorController');
+const AppError = require('./utils/appError');
 
 const app = express();
 
@@ -12,6 +13,13 @@ const userRouter = require('./routes/userRouter');
 
 app.use('/api', todoRouter);
 app.use('/api/user', userRouter);
+app.all('*', (req, res, next) => {
+  // const err = new Error(`Can't find ${req.originalUrl} on the server`);
+  // err.status = 'fail';
+  // err.statusCode = 404;
+
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
 
 app.use(globalErrorHandler);
 
